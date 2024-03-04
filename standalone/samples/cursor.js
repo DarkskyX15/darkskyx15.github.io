4a5b485f3cfde19ed692c85c379fd3887905539f
DarkskyX15 <xu711015@gmail.com>
2024-03-04 16:14:42 +0800

    update learn-css

--- standalone/samples/cursor.js
+++ standalone/samples/cursor.js
@@ -1,14 +1,21 @@
 cursor_left = 0;
 cursor_top = 0;
 var motion_arg = 0.4;
-var point_cnt = 12;
+var point_cnt = 10;
 var inClickable = 0;
 
 addEventListener('load', function(){
-    console.log(navigator.userAgent)
-    cursor_outer = document.getElementById('couter');
+    var onPC = /windows/i.test(navigator.userAgent);
+    if (!onPC) return;
+
+    let cursor_outer = document.createElement('div');
+    cursor_outer.className = 'cursor-outer';
+    cursor_outer.style.top = '0px';
+    cursor_outer.style.left = '0px';
     desttop = cursor_outer.offsetTop;
     destleft = cursor_outer.offsetLeft;
+    document.body.appendChild(cursor_outer);
+
     const selects = document.querySelectorAll('.clickable');
     selects.forEach(element => {
         element.addEventListener('mouseenter', function(){
@@ -22,6 +29,7 @@ addEventListener('load', function(){
             cursor_outer.style.width = '2vmax';
         });
     });
+
     for (let index = 0; index < point_cnt; index++) {
         let dDeg = (360 / point_cnt) * index;
         let newWarp = document.createElement('div');
@@ -32,34 +40,35 @@ addEventListener('load', function(){
         newWarp.appendChild(newIn);
         cursor_outer.appendChild(newWarp);
     }
+
+    addEventListener('mousemove', function(ev){
+        cursor_left = ev.clientX;
+        cursor_top = ev.clientY;
+    })
+    
+    addEventListener('mousedown', (ev)=>{
+        if (ev.button === 0){
+            cursor_outer.style.height = '1.2vmax';
+            cursor_outer.style.width = '1.2vmax';
+        }
+    })
+    
+    addEventListener('mouseup', (ev)=>{
+        if (ev.button === 0){
+            if (inClickable > 0){
+                cursor_outer.style.height = '3.5vmax';
+                cursor_outer.style.width = '3.5vmax';
+            } else{
+                cursor_outer.style.height = '2vmax';
+                cursor_outer.style.width = '2vmax';
+            }
+        }
+    })
+
     setInterval(function(){
         destleft += (cursor_left - destleft) * motion_arg;
         desttop += (cursor_top - desttop) * motion_arg;
         cursor_outer.style.left = destleft + 'px';
         cursor_outer.style.top = desttop + 'px';
     }, 25);
-})
-
-addEventListener('mousemove', function(ev){
-    cursor_left = ev.clientX;
-    cursor_top = ev.clientY;
-})
-
-addEventListener('mousedown', (ev)=>{
-    if (ev.button === 0){
-        cursor_outer.style.height = '1.2vmax';
-        cursor_outer.style.width = '1.2vmax';
-    }
-})
-
-addEventListener('mouseup', (ev)=>{
-    if (ev.button === 0){
-        if (inClickable > 0){
-            cursor_outer.style.height = '3.5vmax';
-            cursor_outer.style.width = '3.5vmax';
-        } else{
-            cursor_outer.style.height = '2vmax';
-            cursor_outer.style.width = '2vmax';
-        }
-    }
 })
