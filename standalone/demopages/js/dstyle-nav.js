966f98c5ef448984823f53420d605b45e5e74f2b
DarkskyX15 <xu711015@gmail.com>
2024-03-18 13:19:51 +0800

    update navigate css & js

--- standalone/demopages/js/dstyle-nav.js
+++ standalone/demopages/js/dstyle-nav.js
@@ -1,26 +1,61 @@
-let DS_navTrigger;
+let DS_navTrigger, DS_navBar; 
+let DS_navStatus = 'top';
+let DS_inTrigger = false, DS_inTriggerClick = 0, DS_ClickReap = 1;
 
+// function 
 
-document.addEventListener('load', () => {
-    
-    if (/windows/i.test(navigator.userAgent)){
-        platform = 'win';
-    }else if (/android/i.test(navigator.userAgent)){
-        platform = 'android';
-    }else if (/iphone/i.test(navigator.userAgent)){
-        platform = 'iphone';
-    }else if (/ipad/i.test(navigator.userAgent)){
-        platform = 'ipad';
-    }else if (/linux/i.test(navigator.userAgent)){
-        platform = 'linux';
-    }
-
-    navTrigger = document.getElementById('nav-trigger');
+addEventListener('load', () => {
 
     if (platform === 'win' || platform === 'linux'){
-
+        DS_ClickReap = 1;               
     }else if (platform != 'unknown'){
-
+        DS_ClickReap = 2;
     }
+
+    DS_navTrigger = document.getElementsByClassName('dstyle-nav-trigger')[0];
+    DS_navBar = document.getElementsByClassName('dstyle-nav')[0];
+
+    if (DS_navStatus === 'top') {
+        if (window.scrollY < 0.1){
+            DS_navBar.classList.remove('dstyle-navTfloat');
+        }else{
+            DS_navBar.classList.add('dstyle-navTfloat');
+        }
+    }
+
+    DS_navTrigger.addEventListener('mouseenter', () => {
+        DS_inTrigger = true;
+    });
+
+    DS_navTrigger.addEventListener('mouseleave', () => {
+        DS_inTrigger = false;
+        DS_inTriggerClick = 0;
+    });
+
+    window.addEventListener('scroll', () => {
+        if (DS_navStatus === 'top') {
+            if (window.scrollY < 0.1){
+                DS_navBar.classList.remove('dstyle-navTfloat');
+            }else{
+                DS_navBar.classList.add('dstyle-navTfloat');
+            }
+        }
+    });
+
+    addEventListener('mousedown', (ev) => {
+        if (ev.button === 0){
+            if (DS_navStatus === 'top'){
+                if (DS_inTrigger) { 
+                    DS_inTriggerClick += 1;
+                }
+                if (DS_inTriggerClick >= DS_ClickReap){
+                    DS_navBar.classList.add('dstyle-navSTbubble');
+                    DS_navBar.classList.remove('dstyle-navTfloat');
+                    DS_navBar.classList.remove('dstyle-navSTtop');
+                    DS_navStatus = 'bubble';
+                }
+            }
+        }
+    });
 
 });
